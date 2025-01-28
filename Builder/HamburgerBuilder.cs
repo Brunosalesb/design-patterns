aab0cde20ab96a8bac90777102c55f892c73fc95
Brunosalesb <brunosb.8zpei@slmail.me>
2025-01-28 15:31:07 -0300

    refactor: adjust create order

--- Builder/HamburgerBuilder.cs
+++ Builder/HamburgerBuilder.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Builder;
 
 public class HamburgerBuilder : SandwichBuilder
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -3,9 +3,9 @@
 Kitchen kitchen = new Kitchen();
 SandwichBuilder b1 = new HamburgerBuilder();
 SandwichBuilder b2 = new FishBuilder();
-		
-kitchen.CreateSandwich(b2);
-b2.GetSandwich();
 
 kitchen.CreateSandwich(b1);
 b1.GetSandwich();
+
+kitchen.CreateSandwich(b2);
+b2.GetSandwich();
